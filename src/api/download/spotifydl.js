815b51c1503abc5d77a7f472d4c3bd29fee8e901
Kenz-Developer <kenzorkut49@gmail.com>
2025-02-26 12:54:51 +0700

    Add files via upload

---
+++ src/api/download/spotifydl.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+module.exports = function (app) {
+    app.get("/search/spotifydl", async (req, res) => {
+        let { q } = req.query;
+        if (!q) return res.status(400).json({ status: false, error: "Query is required" });
+
+        try {
+            let apiKeyData = fs.readFileSync("apikey.json", "utf-8");
+            let apiKeys = JSON.parse(apiKeyData).apikeys;
+            
+            if (!apiKeys.length) throw new Error("No API Key found");
+
+            let randomApiKey = apiKeys[Math.floor(Math.random() * apiKeys.length)];
+
+            let apiUrl = `https://restapi-v2.simplebot.my.id/download/playspotify?apikey=${randomApiKey}&q=${encodeURIComponent(q)}`;
+            let response = await fetch(apiUrl);
+            let data = await response.json();
+
+            if (!data.status) throw new Error(data.error || "Gagal mengambil data");
+
+            res.json({
+                status: true,
+                result: data.result
+            });
+
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+};
