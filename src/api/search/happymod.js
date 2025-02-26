86b2ec85246e7a6870dd6b12d93ea000fa4cf530
Kenz-Developer <kenzorkut49@gmail.com>
2025-02-26 12:37:53 +0700

    Update happymod.js

--- src/api/search/happymod.js
+++ src/api/search/happymod.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 
-module.exports = function (app) {
+module.exports = function(app) {
     app.get("/search/happymod", async (req, res) => {
         let { q } = req.query;
         if (!q) return res.status(400).json({ status: false, error: "Query is required" });
