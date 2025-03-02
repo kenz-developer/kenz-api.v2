00f9904f7fd9e3f9c82abf790a920b8e97dbe3be
Kenz-Developer <kenzorkut49@gmail.com>
2025-03-02 09:05:49 +0700

    Create epep.js

---
+++ src/api/stalking/epep.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+module.exports = function (app) {
+    app.get("/stalking/epep", async (req, res) => {
+        let { id } = req.query;
+        if (!id) {
+            return res.status(400).json({ status: false, error: "ID Free Fire diperlukan!" });
+        }
+
+        try {
+            let response = await fetch(`https://velyn-ererexidchx-api.vercel.app/api/stalk/FreeFireStalk?id=${id}`);
+            let result = await response.json();
+
+            if (!result || !result.status) throw new Error("Gagal mendapatkan data!");
+
+            res.json({
+                status: true,
+                result: result.result
+            });
+
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+};
